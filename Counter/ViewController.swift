3c4b0bd3ac41ca99a8e794021a6304abd3303072
Anton Kuryatin <kuryatinpr@yandex.ru>
2025-11-19 23:08:58 +0300

    Добавил счётчик и кнопку. При нажатии на кнопку значение увеличивается на 1

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,13 +7,20 @@
 
 import UIKit
 
+//final
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var Counter: UILabel!
+    var count: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func pressButton(_ sender: Any) {
+        
+       // print("жмяк") 
+        count += 1
+        Counter.text = "Значение счётчика: \(count)"
+    } 
 }
 
