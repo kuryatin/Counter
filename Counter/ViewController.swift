b29030ccd9f96cc23086cbf963c9b8e66f8755c3
Anton Kuryatin <kuryatinpr@yandex.ru>
2025-11-20 20:32:01 +0300

    Переименовал булит, удалил лишние комментарии, проверил текст на орфографию, исправил другие замечания по ревью

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,23 +7,18 @@
 
 import UIKit
 
-//Объясню, почему в pull request вы не видите всех моих изменений. Дело в том,
-//что я забыл создать отдельную ветку, и влил весь код в main,
-//а уже после снова перечитал правила для сдачи ДЗ. Теперь вот выкручиваюсь.
-
 final class ViewController: UIViewController {
     
-    @IBOutlet weak var Counter: UILabel!
-    var count: Int = 0
+    @IBOutlet weak var meaningOfCounter: UILabel!
+    private var count: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
     @IBAction func pressButton(_ sender: Any) {
         
         print("жмяк")
         count += 1
-        Counter.text = "Значение счётчика: \(count)"
-    } 
+        meaningOfCounter.text = "Значение счётчика: \(count)"
+    }
 }
-
